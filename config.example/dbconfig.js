d785d7de7032e87fbf1c9863f6abf0771e10b37e
Sky <santafejayyt@gmail.com>
2022-09-28 13:40:04 -0700

    a lot

--- config.example/dbconfig.js
+++ config.example/dbconfig.js
@@ -4,7 +4,22 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
+  ci: {
+    client: 'mysql2',
+    connection: {
+      database: 'NECos_ci',
+      user:     'NECos',
+      password: 'necos'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  },
+  
   development: {
     client: 'mysql2',
     connection: {
