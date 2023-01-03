afbdb8cb5669fb0a9c9573f6d6c4dab79812d47c
imskyyc <santafejayyt@gmail.com>
2023-01-03 06:13:14 -0800

    update snyk depcheck

--- src/app/bot/commands/affiliates/affiliates.ts
+++ src/app/bot/commands/affiliates/affiliates.ts
@@ -46,7 +46,8 @@ export default class AffiliatesCommand extends BaseCommand {
         const database: Knex = this.NECos.database;
         const affiliates = await database<Affiliate>("affiliates").select("*");
         const affiliateEmbeds = [];
-        const pages = (affiliates.length > 2 && Math.ceil(affiliates.length / 2)) || 1;
+        const pages =
+          (affiliates.length > 2 && Math.ceil(affiliates.length / 2)) || 1;
         let pageNumber = options.getNumber("page");
 
         if (!pageNumber) {
@@ -55,13 +56,13 @@ export default class AffiliatesCommand extends BaseCommand {
 
         if (pageNumber > pages) {
           pageNumber = pages;
-        } 
+        }
 
         for (const index in affiliates) {
           let start = (pageNumber - 1) * 2;
           let end = pageNumber * 2;
-          let numIndex = parseInt(index)
-          
+          let numIndex = parseInt(index);
+
           if (!(numIndex >= start && numIndex < end)) continue;
 
           const affiliate = affiliates[index];
@@ -199,20 +200,27 @@ export default class AffiliatesCommand extends BaseCommand {
           ];
 
         // Check if group exists
-        const dbAffiliates = await database<Affiliate>("affiliates")
-          .select("*");
+        const dbAffiliates = await database<Affiliate>("affiliates").select(
+          "*"
+        );
 
         let existingAffiliate = undefined;
 
         for (const affiliate of dbAffiliates) {
-          if (affiliate.discord_id == guild.id && affiliate.group_id == groupUrlComponents[0]) {
+          if (
+            affiliate.discord_id == guild.id &&
+            affiliate.group_id == groupUrlComponents[0]
+          ) {
             existingAffiliate = affiliate;
             break;
           }
         }
 
         if (existingAffiliate)
-          return [false, `An affiliate matching groupId ${groupId} already exists!`];
+          return [
+            false,
+            `An affiliate matching groupId ${groupId} already exists!`,
+          ];
 
         // Verify owner of group
         // Get user
@@ -261,14 +269,18 @@ export default class AffiliatesCommand extends BaseCommand {
           owner_id: member.id,
         });
 
-        const representativeRole = guild.roles.cache.find(r => r.name.includes("Group Representative"))
+        const representativeRole = guild.roles.cache.find((r) =>
+          r.name.includes("Group Representative")
+        );
         const errors = [];
 
         if (representativeRole) {
           try {
-            await member.roles.add(representativeRole)
+            await member.roles.add(representativeRole);
           } catch (error) {
-            errors.push(`Failed to add Group Representative role to <@${member.id}>: ${error}`)
+            errors.push(
+              `Failed to add Group Representative role to <@${member.id}>: ${error}`
+            );
           }
         }
 
@@ -276,7 +288,11 @@ export default class AffiliatesCommand extends BaseCommand {
           embeds: [
             this.Bot.createEmbed({
               title: "Affiliate Group Linked",
-              description: `${groupData.name} has successfully been linked to this guild.${(errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""}`,
+              description: `${
+                groupData.name
+              } has successfully been linked to this guild.${
+                (errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""
+              }`,
               color: Colors.Green,
             }),
           ],
@@ -307,43 +323,59 @@ export default class AffiliatesCommand extends BaseCommand {
 
         // Find linked affiliate
         const groupId = options.getInteger("groupid");
-        const dbAffiliates = await database<Affiliate>("affiliates")
-          .select("*");
+        const dbAffiliates = await database<Affiliate>("affiliates").select(
+          "*"
+        );
 
         let affiliate: Affiliate = undefined;
 
         for (const dbAffiliate of dbAffiliates) {
-          if (dbAffiliate.discord_id == guild.id && dbAffiliate.group_id == groupId.toString()) {
+          if (
+            dbAffiliate.discord_id == guild.id &&
+            dbAffiliate.group_id == groupId.toString()
+          ) {
             affiliate = dbAffiliate;
             break;
           }
         }
 
-        if (!affiliate) return [false, `No affiliate was found for this guild matching groupId ${groupId}.`]
-        if (affiliate.owner_id != member.id) return [false, "Only the owner of the group can un-link it."]
+        if (!affiliate)
+          return [
+            false,
+            `No affiliate was found for this guild matching groupId ${groupId}.`,
+          ];
+        if (affiliate.owner_id != member.id)
+          return [false, "Only the owner of the group can un-link it."];
 
         const representatives = JSON.parse(affiliate.representatives);
-        const representativeRole = guild.roles.cache.find(r => r.name.includes("Group Representative"))
+        const representativeRole = guild.roles.cache.find((r) =>
+          r.name.includes("Group Representative")
+        );
         const errors = [];
 
         if (representativeRole) {
           for (const representative of representatives) {
             const guildMember = await guild.members.resolve(representative);
             if (!guildMember) continue;
-  
+
             let isRepresentative = false;
             for (const dbAffiliate of dbAffiliates) {
-              const affiliateRepresentatives = JSON.parse(dbAffiliate.representatives);
-              
-              if (affiliateRepresentatives.includes(guildMember.id)) isRepresentative = true;
+              const affiliateRepresentatives = JSON.parse(
+                dbAffiliate.representatives
+              );
+
+              if (affiliateRepresentatives.includes(guildMember.id))
+                isRepresentative = true;
             }
-  
+
             if (isRepresentative) continue;
 
             try {
-              await guildMember.roles.remove(representativeRole)
+              await guildMember.roles.remove(representativeRole);
             } catch (error) {
-              errors.push(`Failed to remove Group Representative role from <@${guildMember.id}>: ${error}`)
+              errors.push(
+                `Failed to remove Group Representative role from <@${guildMember.id}>: ${error}`
+              );
             }
           }
         }
@@ -352,13 +384,15 @@ export default class AffiliatesCommand extends BaseCommand {
           embeds: [
             this.Bot.createEmbed({
               title: "Affiliate Group Deleted",
-              description: `Your affiliate group has been successfully unlinked from the Discord.${(errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""}`,
-              color: Colors.Green
-            })
-          ]
-        })
+              description: `Your affiliate group has been successfully unlinked from the Discord.${
+                (errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""
+              }`,
+              color: Colors.Green,
+            }),
+          ],
+        });
 
-        return [true, ""]
+        return [true, ""];
       },
     },
 
@@ -391,32 +425,35 @@ export default class AffiliatesCommand extends BaseCommand {
           .select("*")
           .where({
             group_id: groupId.toString(),
-            discord_id: guild.id
+            discord_id: guild.id,
           })
-          .first()
+          .first();
 
-        if (!affiliate) return [false, `No group was found matching groupId ${groupId}.`]
-        if (affiliate.owner_id != member.user.id) return [false, `Only the owner of the group can run the setinvite command.`,]
-        
-        await database("affiliates")
-          .update("invite", invite)
-          .where({
-            group_id: groupId.toString(),
-            discord_id: guild.id
-          })
+        if (!affiliate)
+          return [false, `No group was found matching groupId ${groupId}.`];
+        if (affiliate.owner_id != member.user.id)
+          return [
+            false,
+            `Only the owner of the group can run the setinvite command.`,
+          ];
+
+        await database("affiliates").update("invite", invite).where({
+          group_id: groupId.toString(),
+          discord_id: guild.id,
+        });
 
         await Interaction.editReply({
           embeds: [
             this.Bot.createEmbed({
               title: "Affiliate Invite Updated",
               description: `The invite for ${affiliate.group_name} has been updated to ${invite}.`,
-              color: Colors.Green
-            })
-          ]
-        })
+              color: Colors.Green,
+            }),
+          ],
+        });
 
-        return [true, ""]
-      }
+        return [true, ""];
+      },
     },
 
     {
@@ -443,27 +480,56 @@ export default class AffiliatesCommand extends BaseCommand {
         const database: Knex = this.NECos.database;
         const groupId = options.getInteger("groupid", true);
         const representativeUser = options.getUser("representative", true);
-        const representative = await guild.members.resolve(representativeUser.id);
-        const representativeRole = guild.roles.cache.find(r => r.name.includes("Group Representative"));
+        const representative = await guild.members.resolve(
+          representativeUser.id
+        );
+        const representativeRole = guild.roles.cache.find((r) =>
+          r.name.includes("Group Representative")
+        );
 
-        if (!representative) return [false, `No user matching userId ${representativeUser.id} could be found.`]
-        if (!representativeRole) return [false, "No group representative role could be found. A group representative role must contain the name \"Group Representative\"."]
+        if (!representative)
+          return [
+            false,
+            `No user matching userId ${representativeUser.id} could be found.`,
+          ];
+        if (!representativeRole)
+          return [
+            false,
+            'No group representative role could be found. A group representative role must contain the name "Group Representative".',
+          ];
 
         const affiliate = await database<Affiliate>("affiliates")
           .select("*")
           .where({
             group_id: groupId.toString(),
-            discord_id: guild.id
+            discord_id: guild.id,
           })
-          .first()
+          .first();
 
-        if (!affiliate) return [false, `No group was found matching groupId ${groupId}.`]
-        if (affiliate.owner_id != member.user.id) return [false, "Only the owner of the group can run the addrep command."]
-        
+        if (!affiliate)
+          return [false, `No group was found matching groupId ${groupId}.`];
+        if (affiliate.owner_id != member.user.id)
+          return [
+            false,
+            "Only the owner of the group can run the addrep command.",
+          ];
+
         const representatives = JSON.parse(affiliate.representatives);
-        if (representative.id == affiliate.owner_id) return [false, "The owner of the group cannot be an extra group representative."]
-        if (representatives.length == 2) return [false, "Affiliate Groups can have no more than two extra group representatives."]
-        if (representatives.includes(representative.id)) return [false, `<@${representative.id}> is already a group representative.`]
+        if (representative.id == affiliate.owner_id)
+          return [
+            false,
+            "The owner of the group cannot be an extra group representative.",
+          ];
+        if (representatives.length == 2)
+          return [
+            false,
+            "Affiliate Groups can have no more than two extra group representatives.",
+          ];
+        if (representatives.includes(representative.id))
+          return [
+            false,
+            `<@${representative.id}> is already a group representative.`,
+          ];
 
         representatives.push(representative.id);
 
@@ -471,27 +537,33 @@ export default class AffiliatesCommand extends BaseCommand {
           .update("representatives", JSON.stringify(representatives))
           .where({
             group_id: groupId.toString(),
-            discord_id: guild.id
-          })
+            discord_id: guild.id,
+          });
 
         const errors = [];
         try {
-          await representative.roles.add(representativeRole)
+          await representative.roles.add(representativeRole);
         } catch (error) {
-          errors.push(`Failed to add Group Representative role to <@${representative.id}>: ${error}`)
+          errors.push(
+            `Failed to add Group Representative role to <@${representative.id}>: ${error}`
+          );
         }
 
         await Interaction.editReply({
           embeds: [
             this.Bot.createEmbed({
               title: "Affiliate Representative Added",
-              description: `Successfully added <@${representative.id}> as a group representative.${(errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""}`,
-              color: Colors.Green
-            })
-          ]
-        })
+              description: `Successfully added <@${
+                representative.id
+              }> as a group representative.${
+                (errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""
+              }`,
+              color: Colors.Green,
+            }),
+          ],
+        });
 
-        return [true, ""]
+        return [true, ""];
       },
     },
 
@@ -517,11 +589,15 @@ export default class AffiliatesCommand extends BaseCommand {
           .select("*")
           .where({
             group_id: groupId.toString(),
-            discord_id: guild.id
+            discord_id: guild.id,
           })
-          .first()
+          .first();
 
-        if (!affiliate) return [false, `No affiliate could be found matching groupId ${groupId}.`]
+        if (!affiliate)
+          return [
+            false,
+            `No affiliate could be found matching groupId ${groupId}.`,
+          ];
 
         const representatives = JSON.parse(affiliate.representatives);
         const embeds = [];
@@ -533,18 +609,20 @@ export default class AffiliatesCommand extends BaseCommand {
           const representativeEmbed = new EmbedBuilder()
             .setTitle(guildMember.user.username)
             .setThumbnail(guildMember.user.avatarURL())
-            .setDescription(`Representative of ${affiliate.group_name}.\n(I'd show more data here but Discord has no API Endpoints for it... :)`)
-            .setColor(Colors.Blue)
+            .setDescription(
+              `Representative of ${affiliate.group_name}.\n(I'd show more data here but Discord has no API Endpoints for it... :)`
+            )
+            .setColor(Colors.Blue);
 
           embeds.push(representativeEmbed);
         }
 
         await Interaction.editReply({
           content: `Listing representatives for ${affiliate.group_name}. (${embeds.length}):`,
-          embeds: embeds
-        })
+          embeds: embeds,
+        });
 
-        return [true, ""]
+        return [true, ""];
       },
     },
 
@@ -572,53 +650,82 @@ export default class AffiliatesCommand extends BaseCommand {
         const database: Knex = this.NECos.database;
         const groupId = options.getInteger("groupid", true);
         const representativeUser = options.getUser("representative", true);
-        const representative = await guild.members.resolve(representativeUser.id);
-        const representativeRole = guild.roles.cache.find(r => r.name.includes("Group Representative"));
+        const representative = await guild.members.resolve(
+          representativeUser.id
+        );
+        const representativeRole = guild.roles.cache.find((r) =>
+          r.name.includes("Group Representative")
+        );
 
-        if (!representative) return [false, `No user matching userId ${representativeUser.id} could be found.`]
-        if (!representativeRole) return [false, "No group representative role could be found. A group representative role must contain the name \"Group Representative\"."]
+        if (!representative)
+          return [
+            false,
+            `No user matching userId ${representativeUser.id} could be found.`,
+          ];
+        if (!representativeRole)
+          return [
+            false,
+            'No group representative role could be found. A group representative role must contain the name "Group Representative".',
+          ];
 
         const affiliate = await database<Affiliate>("affiliates")
           .select("*")
           .where({
             group_id: groupId.toString(),
-            discord_id: guild.id
+            discord_id: guild.id,
           })
-          .first()
+          .first();
 
-        if (!affiliate) return [false, `No group was found matching groupId ${groupId}.`]
-        if (affiliate.owner_id != member.user.id) return [false, "Only the owner of the group can run the addrep command."]
-        
-        const representatives: Array<string> = JSON.parse(affiliate.representatives);
-        if (!representatives.includes(representative.id)) return [false, `<@${representative.id}> is not a group representative.`]
+        if (!affiliate)
+          return [false, `No group was found matching groupId ${groupId}.`];
+        if (affiliate.owner_id != member.user.id)
+          return [
+            false,
+            "Only the owner of the group can run the addrep command.",
+          ];
 
-        representatives.splice(representatives.indexOf(representative.id), 1)
+        const representatives: Array<string> = JSON.parse(
+          affiliate.representatives
+        );
+        if (!representatives.includes(representative.id))
+          return [
+            false,
+            `<@${representative.id}> is not a group representative.`,
+          ];
+
+        representatives.splice(representatives.indexOf(representative.id), 1);
 
         await database("affiliates")
           .update("representatives", JSON.stringify(representatives))
           .where({
             group_id: groupId.toString(),
-            discord_id: guild.id
-          })
+            discord_id: guild.id,
+          });
 
         const errors = [];
         try {
-          await representative.roles.remove(representativeRole)
+          await representative.roles.remove(representativeRole);
         } catch (error) {
-          errors.push(`Failed to remove Group Representative role to <@${representative.id}>: ${error}`)
+          errors.push(
+            `Failed to remove Group Representative role to <@${representative.id}>: ${error}`
+          );
         }
 
         await Interaction.editReply({
           embeds: [
             this.Bot.createEmbed({
               title: "Affiliate Representative Removed",
-              description: `Successfully removed <@${representative.id}> as a group representative.${(errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""}`,
-              color: Colors.Green
-            })
-          ]
-        })
+              description: `Successfully removed <@${
+                representative.id
+              }> as a group representative.${
+                (errors.length > 0 && "Errors:\n" + errors.join("\n")) || ""
+              }`,
+              color: Colors.Green,
+            }),
+          ],
+        });
 
-        return [true, ""]
+        return [true, ""];
       },
     },
   ];
